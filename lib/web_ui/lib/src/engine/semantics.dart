1cd70e75b6705a84c79b6edfa8b3075555dc67f9
Bissonhoe <paulovictorbissonho@gmail.comm>
2022-12-15 15:40:30 -0300

    remove live_region semantics

--- lib/web_ui/lib/src/engine/semantics.dart
+++ lib/web_ui/lib/src/engine/semantics.dart
@@ -7,7 +7,7 @@ export 'semantics/accessibility.dart';
 //export 'semantics/image.dart';
 //export 'semantics/incrementable.dart';
 //export 'semantics/label_and_value.dart';
-export 'semantics/live_region.dart';
+//export 'semantics/live_region.dart';
 //export 'semantics/scrollable.dart';
 export 'semantics/semantics.dart';
 export 'semantics/semantics_helper.dart';
